1be74e21c08f4174720d9e88a2c3810e499aae22
Barinder <b.thindd@gmail.com>
2019-03-09 00:19:15 -0800

    NN in R using Python

--- Reticulate_Code.R
+++ Reticulate_Code.R
@@ -3,31 +3,75 @@
 # R libraries
 library(reticulate)
 library(tidyverse)
-library(reticulate)
-library(tensorflow)
-library(keras)
+
+# Looking at python version
 py_config()
 
 # Python libraries
 test = import("numpy")
 
-
 # Looking at directory in R using os
 os <- import("os")
 os$listdir(".")
 
+
+## RUNNING PYTHON IN R
 repl_python()
 
+## Libraries 
 import pandas as pd
-import torch
+import numpy as np
+import matplotlib.pyplot as plt   #Data visualisation libraries 
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
-crime = pd.read_csv("Crime.csv", encoding='utf-8') 
+## Setting wd
+from os import chdir, getcwd
+wd = getcwd()
+chdir(wd)
 
-crime.head()
+## Reading in data
+USAhousing = pd.read_csv('USA_Housing.csv')
+
+## Looking at data
+USAhousing.head()
+USAhousing.info()
+USAhousing.describe()
+USAhousing.columns
+
+## Looking at pairs plot
+sns.pairplot(USAhousing)
+
+## Looking at distribution of Price
+sns.distplot(USAhousing['Price'])
+
+## Looking at correlations
+USAhousing.corr()
+
+## Splitting into response and predictors
+X = USAhousing[['Avg. Area Income', 'Avg. Area House Age', 'Avg. Area Number of Rooms',
+                'Avg. Area Number of Bedrooms', 'Area Population']]
+y = USAhousing['Price']
+
+## Splitting in to test and train
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=101)
+
+## Running regression
+lm = LinearRegression()
+lm.fit(X_train,y_train)
+
+## Predictions
+predictions = lm.predict(X_test)
+
+## Looking at predictions vs. actual
+plt.scatter(y_test,predictions)
 
 exit
-# 
+## EXITING PYTHON IN R
 
 ggplot(py$data, aes(x = year, y = wloc)) + geom_point()
+
+py$USAhousing
 py$crime
 py$data[1]
